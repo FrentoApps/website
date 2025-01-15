b410c294f18ea9c69ec90f0b912c5d2a8a12b996
Zedan <anjanilearns@gmail.com>
2025-01-15 14:00:42 +0000

    Converted to Static

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  output: 'export'
+};
 
 export default nextConfig;
